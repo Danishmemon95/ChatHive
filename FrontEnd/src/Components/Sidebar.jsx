04167acd79185069cdee8007710d6285eedac2e4
Danishmemon95 <danishmemon9558@gmail.com>
2025-07-05 20:45:15 +0530

    fix: correct casing in import path for SidebarSkeleton component

--- FrontEnd/src/Components/Sidebar.jsx
+++ FrontEnd/src/Components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useChatStore } from "../store/useChatStore";
 import { useAuthStore } from "../store/useAuthStore";
-import SidebarSkeleton from "./skeleton/SidebarSkeleton";
+import SidebarSkeleton from "./Skeleton/SidebarSkeleton";
 import { Users } from "lucide-react";
 
 const Sidebar = () => {
